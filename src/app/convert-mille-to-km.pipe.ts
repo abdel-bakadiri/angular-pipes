874ee44380d03c702ebdb960e7fe95e4b1dc6e17
abdel <abdelouahab.bakadiri@gmail.com>
2022-02-26 22:36:48 +0100

    cutom the pipe

--- src/app/convert-mille-to-km.pipe.ts
+++ src/app/convert-mille-to-km.pipe.ts
@@ -4,10 +4,19 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'convertMilleToKm',
 })
 export class ConvertMilleToKmPipe implements PipeTransform {
-  transform(value: any, ...args: unknown[]): number {
+  transform(value: any, target: string): number {
     if (!value) {
       return 0;
     }
-    return parseInt(value) * 1.60934;
+    switch (target) {
+      case 'km':
+        return parseInt(value) * 1.60934;
+      case 'm':
+        return parseInt(value) * 1.60934 * 1000;
+      case 'cm':
+        return parseInt(value) * 1.60934 * 1000 * 100;
+      default:
+        throw new Error('Please put a valid unit of mesure');
+    }
   }
 }
